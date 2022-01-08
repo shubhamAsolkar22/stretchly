d36d0b1904e3284cac9a460e08d75e89d0216fbe
shubham <shubham@fedora>
2022-01-08 17:25:02 +0530

    added test class

---
+++ src/test/java/com/fkog/stretchly/TestStretchly.java
@@ -0,0 +1,9 @@
+package com.fkog.stretchly;
+
+public class TestStretchly {
+
+	public static void main(String[] args) throws InterruptedException {
+		StretchlyApp.main(new String[] {"20"});
+	}
+
+}
